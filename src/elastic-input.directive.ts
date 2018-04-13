4714886152e86381028f30b205dab17f101707dd
Anirudh Varma <anirudh@spotdraft.com>
2018-04-13 14:21:19 +0530

    Repaint only if the value changes.

--- src/elastic-input.directive.ts
+++ src/elastic-input.directive.ts
@@ -1,4 +1,4 @@
-import {ElementRef, HostListener, Directive, OnInit} from '@angular/core';
+import { ElementRef, HostListener, Directive, OnInit } from '@angular/core';
 
 @Directive({
   selector: 'input[elastic-input]'
@@ -6,17 +6,16 @@ import {ElementRef, HostListener, Directive, OnInit} from '@angular/core';
 export class ElasticInputDirective implements OnInit {
   wrapper: HTMLElement;
   mirror: HTMLElement;
+  content: any;
 
   @HostListener('input', ['$event.target'])
-
-  onInput (): void {
+  onInput(): void {
     this.update();
   }
 
-  constructor (private element: ElementRef) {
-  }
+  constructor(private element: ElementRef) {}
 
-  ngOnInit (): void {
+  ngOnInit(): void {
     this.wrapper = document.createElement('div');
     this.wrapper.style.position = 'fixed';
     this.wrapper.style.top = '-999px';
@@ -33,7 +32,7 @@ export class ElasticInputDirective implements OnInit {
     this.update();
   }
 
-  ngAfterContentChecked (): void {
+  ngAfterContentChecked(): void {
     this.update();
   }
 
@@ -41,37 +40,55 @@ export class ElasticInputDirective implements OnInit {
     this.update();
   }
 
-  ngOnDestroy (): void {
+  ngOnDestroy(): void {
     this.mirror.remove();
     this.wrapper.remove();
   }
 
-  setMirrorStyle (mirror: HTMLElement, element: HTMLElement) {
+  setMirrorStyle(mirror: HTMLElement, element: HTMLElement) {
     const style = window.getComputedStyle(element);
 
-    ['fontFamily', 'fontSize', 'fontWeight', 'fontStyle',
-      'letterSpacing', 'textTransform', 'wordSpacing'].forEach(
-      (value) => {
-        mirror.style[value] = style[value];
-      });
+    [
+      'fontFamily',
+      'fontSize',
+      'fontWeight',
+      'fontStyle',
+      'letterSpacing',
+      'textTransform',
+      'wordSpacing'
+    ].forEach(value => {
+      mirror.style[value] = style[value];
+    });
 
     mirror.style.paddingLeft = style.textIndent;
 
     if (style.boxSizing === 'border-box') {
-      ['paddingLeft', 'paddingRight', 'borderLeftStyle', 'borderLeftWidth',
-        'borderRightStyle', 'borderRightWidth'].forEach((value) => {
+      [
+        'paddingLeft',
+        'paddingRight',
+        'borderLeftStyle',
+        'borderLeftWidth',
+        'borderRightStyle',
+        'borderRightWidth'
+      ].forEach(value => {
         mirror.style[value] = style[value];
       });
     } else if (style.boxSizing === 'padding-box') {
-      ['paddingLeft', 'paddingRight'].forEach((value) => {
+      ['paddingLeft', 'paddingRight'].forEach(value => {
         mirror.style[value] = style[value];
       });
     }
   }
 
-  update () {
+  update() {
     const domEl = this.element.nativeElement;
-    this.mirror.innerText = domEl.value || domEl.placeholder;
+    const value = domEl.value || domEl.placeholder;
+    if (this.content != value) {
+      this.content = value;
+    } else {
+      return;
+    }
+    this.mirror.innerText = value;
     const delta = 1;
     domEl.style.width = `${this.mirror.offsetWidth + delta}px`;
   }
